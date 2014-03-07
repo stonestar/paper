1f9c6dcf11f8c8ecdab8bab917a76fc5ce43034a
stonestar <yangjunhuangwan@gmail.com>
2014-03-07 15:59:53 +0800

    add login

---
+++ ababy/Baby/src/com/yangjun/baby/activity/LoginMainActivity.java
@@ -0,0 +1,17 @@
+package com.yangjun.baby.activity;
+
+import com.yangjun.baby.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginMainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.login_main);
+	}
+
+}
